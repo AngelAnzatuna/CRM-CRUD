5fbc6fb050ea87d3c2aacd1afecace1fe3095c56
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-11 01:44:05 +0100

    feat: actualizar datos de un cliente

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,7 @@
 export function mostrarAlerta(mensaje) {
     const alerta = document.querySelector('.bg-red-100');
 
-    if(!alerta) {
+    if (!alerta) {
         const alerta = document.createElement('p');
 
         alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
@@ -18,4 +18,8 @@ export function mostrarAlerta(mensaje) {
             alerta.remove();
         }, 3000);
     }
+}
+
+export function validar(obj) {
+    return !Object.values(obj).every(input => input !== '');
 }
